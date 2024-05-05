7b23d417a57b5641e5570a97ca3eb388fe58a93d
haido86 <thithuhai.do@gmail.com>
2024-05-05 16:19:13 +0300

    Adding metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  return { title: `Edit Invoice - ${params.id}` };
+}
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
